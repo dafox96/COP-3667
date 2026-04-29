64a9fa82c75c7ecf1d02d9407625f1f1e5447048
David Fox <davidlorenfox@gmail.com>
2026-04-28 20:46:11 -0400

    Assignment 6

---
+++ basic-android-kotlin-compose-training-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/data/ScheduleDao.kt
@@ -0,0 +1,14 @@
+package com.example.busschedule.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ScheduleDao {
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getSchedule(): Flow<List<BusSchedule>>
+
+    @Query("SELECT * FROM schedule WHERE stop_name = :name")
+    fun getStop(name: String): Flow<List<BusSchedule>>
+}
